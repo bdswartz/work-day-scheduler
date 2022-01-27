124c0b2a5ef2fd48108f687b1ecc510628500f53
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-01-27 09:41:20 -0500

    edited classes, added some styling and started click events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,42 @@
 var date = moment().format("dddd, MMMM Do");
-var dayStart = 13
+var dayStartTime = 9;
 
-setInterval(function() {
+var showCalendar = function() {
     $("#currentDay").text(date);
     console.log(date);
     $(".hour").each(function(index) {
-        var time = moment().set("hour", dayStart + index );
-        var displayTime = moment().set("hour", dayStart + index ).format("hA");
+        var time = moment().set("hour", dayStartTime + index );
+        var displayTime = moment().set("hour", dayStartTime + index ).format("hA");
         $(this).text(displayTime);
-            if (moment().isAfter(time,"hour")) {
-                console.log("true");
-                $(this).siblings(".col-10").addClass("past");
-            }
-            else if (moment().isSame(time,"hour")) {
-                console.log("true");
-                $(this).siblings(".col-10").addClass("present");
-            }
-            else if (moment().isBefore(time,"hour")) {
-                console.log("true");
-                $(this).siblings(".col-10").addClass("future");
-            }
+        if (moment().isAfter(time,"hour")) {
+            console.log("true");
+            $(this).siblings(".col-10").addClass("past");
+        }
+        else if (moment().isSame(time,"hour")) {
+            console.log("true");
+            $(this).siblings(".col-10").addClass("present");
+        }
+        else if (moment().isBefore(time,"hour")) {
+            console.log("true");
+            $(this).siblings(".col-10").addClass("future");
+        }
     });
-} , 60000);
+};
+
+$(".task-item").on("click", "p", function() {
+    console.log("here");
+    // get current text of p element
+    var text = $(this)
+      .text()
+      .trim();
+  
+    // replace p element with a new textarea
+    var textInput = $("<textarea>").val(text);
+    $(this).replaceWith(textInput);
+  
+    // auto focus new element
+    textInput.trigger("focus");
+  });
+
+showCalendar();
+setInterval(showCalendar, 60000);
