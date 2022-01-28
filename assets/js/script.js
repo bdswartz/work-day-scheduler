2cb76be70e665c678ac7093c5c361a45b667a949
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-01-28 00:33:31 -0500

    load from and save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,45 @@
 var date = moment().format("dddd, MMMM Do");
 var dayStartTime = 9;
+var taskData = [];
+
+var loadTasks = function() {
+    taskData = JSON.parse(localStorage.getItem("workScheduler"));
+    $(".task-item").each(function(index) {
+        $(this).val(taskData[index]);
+    });
+};
+
+var saveTasks = function() {
+    $(".task-item").each(function(index) {
+        taskData[index] = $(this).val();
+    });
+    localStorage.setItem("workScheduler", JSON.stringify(taskData));
+};
+
 
 var showCalendar = function() {
     $("#currentDay").text(date);
-    console.log(date);
     $(".hour").each(function(index) {
         var time = moment().set("hour", dayStartTime + index );
         var displayTime = moment().set("hour", dayStartTime + index ).format("hA");
         $(this).text(displayTime);
         if (moment().isAfter(time,"hour")) {
-            console.log("true");
-            $(this).siblings(".col-10").addClass("past");
-        }
-        else if (moment().isSame(time,"hour")) {
-            console.log("true");
-            $(this).siblings(".col-10").addClass("present");
+            $(this).siblings(".task-item").addClass("past");
+            $(this).siblings(".task-item").removeClass("present future");
         }
         else if (moment().isBefore(time,"hour")) {
-            console.log("true");
-            $(this).siblings(".col-10").addClass("future");
+            $(this).siblings(".task-item").addClass("future");
+            $(this).siblings(".task-item").removeClass("present past");
+        }
+        else {
+            $(this).siblings(".task-item").addClass("present");
+            $(this).siblings(".task-item").removeClass("past future");
         }
     });
 };
 
-$(".task-item").on("click", "p", function() {
-    console.log("here");
-    // get current text of p element
-    var text = $(this)
-      .text()
-      .trim();
-  
-    // replace p element with a new textarea
-    var textInput = $("<textarea>").val(text);
-    $(this).replaceWith(textInput);
-  
-    // auto focus new element
-    textInput.trigger("focus");
-  });
+  $(".saveBtn").on("click", saveTasks);
 
+loadTasks();
 showCalendar();
-setInterval(showCalendar, 60000);
+setInterval(showCalendar, 300000);
